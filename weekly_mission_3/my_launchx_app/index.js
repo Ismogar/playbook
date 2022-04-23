b823fef11daef86180da73aee423f58bb0fabb1f
Ismogar <hakutoishiwashi@gmail.com>
2022-04-22 20:04:48 -0500

    practica 1

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,10 @@
+const MissionCommander = require('./app/missionCommander');
+
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+
+const mario = new MissionCommander("Mario")
+console.log(mario.name)
+
+const luigi = new MissionCommander("Luigi")
+console.log(luigi.name)
